c36c175b546c95673d63832dedaca312c3d2fd90
sadhana1058 <sadhana1058@yahoo.in>
2021-12-05 15:02:35 +0530

    added misclassification file

---
+++ src/misclassification.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision
+
+
+#display the misclassified images
+def show_misclassified_images(model, device, dataset, classes):
+  misclassified_images = []
+  
+  for images, labels in dataset:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            for i in range(len(labels)):
+              if(len(misclassified_images)<25 and predicted[i]!=labels[i]):
+                misclassified_images.append([images[i],predicted[i],labels[i]])
+            if(len(misclassified_images)>25):
+              break
+    
+  
+  fig = plt.figure(figsize = (8,8))
+  for i in range(25):
+        sub = fig.add_subplot(5, 5, i+1)
+        img = misclassified_images[i][0].cpu()
+        img = img / 2 + 0.5 
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg,(1, 2, 0)),interpolation='none')
+        
+        sub.set_title("P={}, A={}".format(str(classes[misclassified_images[i][1].data.cpu().numpy()]),str(classes[misclassified_images[i][2].data.cpu().numpy()])))
+        
+  plt.tight_layout()
